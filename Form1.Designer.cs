6b5b558b1b879bd17743860f0ce9fdfac5a04909
RicardoCS-CPNV <ricardo.costa1@eduvaud.ch>
2024-01-15 08:42:41 +0100

    Test transfet 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,16 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Title = new System.Windows.Forms.Label();
             this.ScoreText = new System.Windows.Forms.Label();
             this.Score = new System.Windows.Forms.Label();
+            this.pnlQuitter = new System.Windows.Forms.Panel();
+            this.pnlWin = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlLose = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlWin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.pnlLose.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Title
             // 
             this.Title.AutoSize = true;
             this.Title.Font = new System.Drawing.Font("Arial Rounded MT Bold", 63.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(12, 10);
+            this.Title.Location = new System.Drawing.Point(130, 10);
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(246, 98);
             this.Title.TabIndex = 0;
@@ -46,8 +58,9 @@
             // ScoreText
             // 
             this.ScoreText.AutoSize = true;
+            this.ScoreText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ScoreText.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ScoreText.Location = new System.Drawing.Point(264, 22);
+            this.ScoreText.Location = new System.Drawing.Point(382, 22);
             this.ScoreText.Name = "ScoreText";
             this.ScoreText.Size = new System.Drawing.Size(147, 43);
             this.ScoreText.TabIndex = 1;
@@ -56,25 +69,116 @@
             // Score
             // 
             this.Score.AutoSize = true;
-            this.Score.BackColor = System.Drawing.Color.Transparent;
+            this.Score.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.Score.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Score.Location = new System.Drawing.Point(264, 57);
+            this.Score.Location = new System.Drawing.Point(382, 57);
             this.Score.Margin = new System.Windows.Forms.Padding(0);
+            this.Score.MinimumSize = new System.Drawing.Size(147, 0);
             this.Score.Name = "Score";
-            this.Score.Size = new System.Drawing.Size(41, 43);
+            this.Score.Size = new System.Drawing.Size(147, 43);
             this.Score.TabIndex = 2;
             this.Score.Text = "0";
+            // 
+            // pnlQuitter
+            // 
+            this.pnlQuitter.Location = new System.Drawing.Point(281, 569);
+            this.pnlQuitter.Name = "pnlQuitter";
+            this.pnlQuitter.Size = new System.Drawing.Size(125, 55);
+            this.pnlQuitter.TabIndex = 3;
+            // 
+            // pnlWin
+            // 
+            this.pnlWin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(180)))));
+            this.pnlWin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlWin.Controls.Add(this.pictureBox2);
+            this.pnlWin.Controls.Add(this.label1);
+            this.pnlWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlWin.Location = new System.Drawing.Point(81, 211);
+            this.pnlWin.Name = "pnlWin";
+            this.pnlWin.Size = new System.Drawing.Size(537, 239);
+            this.pnlWin.TabIndex = 4;
+            this.pnlWin.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-9, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(186, 168);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 74);
+            this.label1.MaximumSize = new System.Drawing.Size(400, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tu as gagné !!!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlLose
+            // 
+            this.pnlLose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(180)))));
+            this.pnlLose.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLose.Controls.Add(this.label3);
+            this.pnlLose.Controls.Add(this.pictureBox1);
+            this.pnlLose.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlLose.Location = new System.Drawing.Point(81, 211);
+            this.pnlLose.Name = "pnlLose";
+            this.pnlLose.Size = new System.Drawing.Size(537, 239);
+            this.pnlLose.TabIndex = 5;
+            this.pnlLose.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlLose_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(200, 59);
+            this.label3.MaximumSize = new System.Drawing.Size(400, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 40);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tu as perdu ;(";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 571);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(675, 636);
+            this.Controls.Add(this.pnlLose);
+            this.Controls.Add(this.pnlWin);
+            this.Controls.Add(this.pnlQuitter);
             this.Controls.Add(this.Score);
             this.Controls.Add(this.ScoreText);
             this.Controls.Add(this.Title);
             this.Name = "Form1";
             this.Text = "2048";
+            this.pnlWin.ResumeLayout(false);
+            this.pnlWin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.pnlLose.ResumeLayout(false);
+            this.pnlLose.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +189,13 @@
         private System.Windows.Forms.Label Title;
         private System.Windows.Forms.Label ScoreText;
         private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Panel pnlQuitter;
+        private System.Windows.Forms.Panel pnlWin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlLose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,77 +1,126 @@
-﻿using System;
+﻿using ePOSOne.btnProduct;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Channels;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace _2048_WindowsForm
 {
     public partial class Form1 : Form
     {
-        static Label[,] labels = new Label[4, 4]; //Création du tableau de labels
-        static int[,] table = new int[4, 4]; //Création du tableau ud jeu
-        static Color[] color = { Color.Snow, Color.Beige, Color.MistyRose, Color.SandyBrown, Color.Orange, Color.Tomato, };
+        static System.Windows.Forms.Label[,] labels = new System.Windows.Forms.Label[4, 4];
+        static int[,] table = new int[4, 4];
+        static Color[] color = { Color.Snow, Color.AntiqueWhite, Color.NavajoWhite, Color.BurlyWood, Color.Goldenrod, Color.Chocolate, Color.SaddleBrown, Color.Brown, Color.AliceBlue, Color.LightGray, Color.Gray, Color.LightSlateGray, Color.DeepPink };
         static int score = 0;
+        bool Defaite = false;
+        static bool win = false;
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox2.BackColor = Color.Transparent;
+
+            pnlLose.Visible = false;
             InitJeu();
+            this.KeyPreview = true;
+
+            this.KeyDown += new KeyEventHandler((sender, e) =>
+            {
+                // Le contenu du gestionnaire d'événements KeyDown
+                if (!Defaite)
+                {
+                    switch (e.KeyData)
+                    {
+                        case Keys.Left:
+                        case Keys.Up:
+                        case Keys.Right:
+                        case Keys.Down:
+                            bool tableChange = DetectionFleche(e.KeyData);
+                            if (tableChange)
+                            {
+                                Score.Text = score.ToString();
+                                NombreAleatoire();
+                            }
+
+                            Affiche();
+                            Victoire();
+                            if (!TableauRempli())
+                            {
+                                Defaite = true;
+                            }
+                            break;
+
+                        case Keys.C:
+                            Application.Exit();
+                            break;
+                    }
+                }
+                else
+                {
+                    pnlLose.Visible = true;
+                    if (e.KeyData == Keys.C)
+                    {
+                        Application.Exit();
+                    }
+                }
+            });
         }
 
         public void InitJeu()
         {
+
+
             for (int ligne = 0; ligne < table.GetLength(0); ligne++)
             {
-                for (int colonne = 0; colonne < table.GetLength(0); colonne++)
+                for (int colonne = 0; colonne < table.GetLength(1); colonne++)
                 {
-                    labels[ligne, colonne] = new Label(); //Création du tableau
-
-                    //Le 20 + 100 * colonne détermine le placement du premier label
-                    //Le 20 + 100 * ligne détermine le placement du label en Y
-                    //Le 90, 90 est la taille du label
-                    labels[ligne, colonne].Bounds = new Rectangle(15 + 110 * colonne, 130 + 110 * ligne, 100, 100);
-
-                    //Met le texte au centre du label
-                    labels[ligne, colonne].TextAlign = ContentAlignment.MiddleCenter;
-                    labels[ligne, colonne].Font = new Font("Arial", 20, FontStyle.Bold);
+                    labels[ligne, colonne] = new System.Windows.Forms.Label();
+                    labels[ligne, colonne].Bounds = new Rectangle(130 + 110 * colonne, 120 + 110 * ligne, 105, 105);
+                    labels[ligne, colonne].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    labels[ligne, colonne].Font = new Font("Arial Rounded MT Bold", 22, FontStyle.Bold);
 
                     Controls.Add(labels[ligne, colonne]);
                 }
             }
-            //Génlration des 2 nombres aléatoires
+            Panel background = new Panel();
+            background.BackColor = Color.Salmon;
+            background.Size = new Size(455, 455);
+            background.Location = new Point(120, 110);
+            Controls.Add(background);
+
             for (int i = 0; i < 2; i++)
             {
                 NombreAleatoire();
-                affiche();
+                Affiche();
             }
-            
         }
 
-        static void affiche()
+        private void Affiche()
         {
-            for (int ligne = 0;ligne < table.GetLength(0); ligne++)
+            //réaffiche tout le tableau avec les bonnes couleurs et les bons textes, conformément au tableau jeu
+            for (int ligne = 0; ligne < 4; ligne++)
             {
-                for (int colonne = 0;colonne < table.GetLength(0); colonne++)
+                for (int colonne = 0; colonne < 4; colonne++)
                 {
                     if (table[ligne, colonne] > 0)
-                    {
-                        labels[ligne, colonne].Text = table[ligne, colonne].ToString();
-                    }
+                        labels[ligne, colonne].Text = (table[ligne, colonne]).ToString();
                     else
-                    {
                         labels[ligne, colonne].Text = "";
-                    }
-                    labels[ligne, colonne].BackColor = color[table[ligne, colonne]];
+                    labels[ligne, colonne].BackColor = Couleur(table[ligne, colonne]);
                 }
             }
         }
 
-        //Créer un nombre aléatoire et selectionne une tuile aléatoire
-        static void NombreAleatoire()
+        private Color Couleur(int nombre)
+        {
+            int index = (int)Math.Log(nombre, 2);
+            return index >= 0 && index < color.Length ? color[index] : Color.Snow;
+        }
+
+        private void NombreAleatoire()
         {
             Random random = new Random();
             int randomNumber2 = (random.Next(10) == 0) ? 4 : 2;
@@ -95,8 +144,7 @@ namespace _2048_WindowsForm
             }
         }
 
-        //Regarde si la valeur est de 0
-        static bool AjoutNombrePossible()
+        private bool AjoutNombrePossible()
         {
             for (int row = 0; row < table.GetLength(0); row++)
             {
@@ -111,42 +159,13 @@ namespace _2048_WindowsForm
             return false;
         }
 
-
-        /*public void newVide()
+        private bool DetectionFleche(Keys key)
         {
-            //Génération d'un tableau des cases vides, de 0 à 16
-            List<int> aVide = new List<int>();
-            for (int ligne = 0; ligne < table.GetLength(0); ligne++)
-            {
-                for (int colonne = 0; colonne < table.GetLength(0); colonne++)
-                {
-                    if (table[ligne, colonne] == 0)
-                    {
-                        aVide.Add(4 * ligne + colonne); //Stocker le num de case
-                    }
-                }
-            }
-
-            //Prendre une de ces cases aléatoirement
-            if (aVide.Count > 0)
-            {
-                Random aleatoire = new Random();
-                int nalea = aleatoire.Next(aVide.Count); //Génère un entier aléatoire
-                table[aVide[nalea] / 4, aVide[nalea] % 4] = 1; //Met un 2 (2 puissamce 1) dans la case choisie
-                affiche();
-            }
-
-        }*/
-
-        //Détection des flèches
-        static bool DetectionFleche(ConsoleKey key)
-        {
-            int[,] tableTest = (int[,])table.Clone(); //Créer une copie du tableau avant de faire le mouvement
+            int[,] tableTest = (int[,])table.Clone();
             int[] table1D;
             switch (key)
             {
-                //Flêche du haut
-                case ConsoleKey.UpArrow:
+                case Keys.Up:
                     for (int i = 0; i < 4; i++)
                     {
                         table1D = MouvementFusion(table[0, i], table[1, i], table[2, i], table[3, i]);
@@ -155,11 +174,10 @@ namespace _2048_WindowsForm
                         table[2, i] = table1D[2];
                         table[3, i] = table1D[3];
                     }
-                    affiche();
+                    Affiche();
                     break;
 
-                //Flêche du bas
-                case ConsoleKey.DownArrow:
+                case Keys.Down:
                     for (int i = 0; i < 4; i++)
                     {
                         table1D = MouvementFusion(table[3, i], table[2, i], table[1, i], table[0, i]);
@@ -168,11 +186,10 @@ namespace _2048_WindowsForm
                         table[2, i] = table1D[1];
                         table[3, i] = table1D[0];
                     }
-                    affiche();
+                    Affiche();
                     break;
 
-                //Flêche de gauche
-                case ConsoleKey.LeftArrow:
+                case Keys.Left:
                     for (int i = 0; i < 4; i++)
                     {
                         table1D = MouvementFusion(table[i, 0], table[i, 1], table[i, 2], table[i, 3]);
@@ -181,11 +198,10 @@ namespace _2048_WindowsForm
                         table[i, 2] = table1D[2];
                         table[i, 3] = table1D[3];
                     }
-                    affiche();
+                    Affiche();
                     break;
 
-                //Flêche de droite
-                case ConsoleKey.RightArrow:
+                case Keys.Right:
                     for (int i = 0; i < 4; i++)
                     {
                         table1D = MouvementFusion(table[i, 3], table[i, 2], table[i, 1], table[i, 0]);
@@ -194,25 +210,14 @@ namespace _2048_WindowsForm
                         table[i, 2] = table1D[1];
                         table[i, 3] = table1D[0];
                     }
-                    affiche();
-                    break;
-
-                //C quitte le programme
-                case ConsoleKey.C:
-                    break;
-
-                //Affiche un message d'erreur et demande d'appuyer sur une flêche
-                default:
-                    Console.WriteLine("Veuillez taper une touche valide.");
+                    Affiche();
                     break;
             }
-            return ChangementTable(tableTest, table); //Verifie si le tableau a été modifié après le mouvement
+            return ChangementTable(tableTest, table);
         }
 
-        //Gère le mouvement et la fusion des tuiles
-        static int[] MouvementFusion(int nb0, int nb1, int nb2, int nb3)
+        private int[] MouvementFusion(int nb0, int nb1, int nb2, int nb3)
         {
-            //Gère le mouvement
             if (nb2 == 0 && nb3 > 0)
             {
                 nb2 = nb3;
@@ -231,7 +236,7 @@ namespace _2048_WindowsForm
                 nb2 = nb3;
                 nb3 = 0;
             }
-            //Gère la fusion
+
             if (nb0 == nb1)
             {
                 nb0 += nb1;
@@ -253,33 +258,31 @@ namespace _2048_WindowsForm
                 nb3 = 0;
                 score += nb2;
             }
+
             int[] tableau = { nb0, nb1, nb2, nb3 };
             return tableau;
         }
 
-        //Verifie si le tableau est remplie et si des mouvements sont encores possible
-        static bool TableauRempli()
+        private bool TableauRempli()
         {
             for (int row = 0; row < table.GetLength(0); row++)
             {
                 for (int col = 0; col < table.GetLength(1); col++)
                 {
                     if (table[row, col] == 0)
-                        return true; //Trouve un 0 dans le tableau
+                        return true;
 
                     if (row < table.GetLength(0) - 1 && table[row, col] == table[row + 1, col])
-                        return true; //Mouvement Verticale possible
+                        return true;
 
                     if (col < table.GetLength(1) - 1 && table[row, col] == table[row, col + 1])
-                        return true; //M ouvement Horizontale possible
+                        return true;
                 }
             }
-
             return false;
         }
 
-        //Verifie si le tableau est identique ou non
-        static bool ChangementTable(int[,] tableOriginal, int[,] tableCourante)
+        private bool ChangementTable(int[,] tableOriginal, int[,] tableCourante)
         {
             for (int row = 0; row < tableOriginal.GetLength(0); row++)
             {
@@ -294,21 +297,58 @@ namespace _2048_WindowsForm
             return false;
         }
 
-        //Verifie la victoire
-        static bool Victoire()
+        private bool Victoire()
         {
             for (int row = 0; row < table.GetLength(0); row++)
             {
                 for (int col = 0; col < table.GetLength(1); col++)
                 {
-                    if (table[row, col] >= 2048)
-                        return true;
+                    if (table[row, col] == 2048 && win == false)
+                    {
+                        win = true;
+                        pnlWin.Visible = true;
+                    }
                 }
             }
             return false;
         }
 
+        private void button_WOC1_Click(object sender, EventArgs e)
+        {
 
+            Application.Exit();
+        }
 
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            pnlWin.Visible = false;
+        }
+
+        private void button_WOC1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnQuitterLose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Mettre une bordure au panel de défaite
+        private void pnlLose_Paint(object sender, PaintEventArgs e)
+        {
+            if (pnlLose.BorderStyle == BorderStyle.FixedSingle)
+                {
+                    int thickness = 6;
+                    int halfThickness = thickness / 2;
+                    using (Pen p = new Pen(Color.IndianRed, thickness))
+                    {
+                        e.Graphics.DrawRectangle(p, new Rectangle(halfThickness,
+                                                                  halfThickness,
+                                                                  pnlLose.ClientSize.Width - thickness,
+                                                                  pnlLose.ClientSize.Height - thickness));
+                    }
+            }
+        }
     }
 }
